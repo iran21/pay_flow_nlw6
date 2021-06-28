2b33f3e6c33c5eaefd5780111d122122533520b4
Iran Neto <iransneto2@gmail.com>
2021-06-28 16:17:04 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/auth/login_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/noconexion/noconexion_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/theme/app_colors.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFirebase());
+}
+
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({ Key? key }) : super(key: key);
+
+  @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+
+final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+   @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return NoConexionPage();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
 
 class AppWidget extends StatelessWidget {
